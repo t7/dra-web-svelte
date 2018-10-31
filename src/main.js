6e5b96e143ee0b901d0527983b1c2a39e3664bd5
sburke0708 <sburke@tandemseven.com>
2018-10-31 15:43:50 -0400

    add fetch for weather

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import App from './App.html';
-import { locationStore } from './stores';
 
 const app = new App({
 	target: document.body,
-	data: {
-		name: 'worldsss'
-	},
-	store: locationStore
 });
 
 window.app = app;
