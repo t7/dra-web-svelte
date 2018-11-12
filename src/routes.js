eb97cd13761650b8a3605165900f62c431f2989f
sburke0708 <sburke@tandemseven.com>
2018-11-12 10:42:13 -0500

    add settings route

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import Home from './pages/Home.html';
+import Settings from './pages/Settings.html';
+
+export default {
+  mode: 'hash',
+  routes: {
+    '/': Home,
+    '/settings': Settings
+  }
+}
