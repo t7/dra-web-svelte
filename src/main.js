c0806ae5784664c8cb7810530018d28b860be1bf
sburke0708 <sburke@tandemseven.com>
2018-11-14 20:26:54 -0500

    improve styles

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
 import App from './App.html';
-import { globalStore } from './stores/globalStore';
 
-const app = new App({
-	target: document.body,
-	store: globalStore
-});
-
-window.app = app;
-
-export default app;
+export default new App({ target: document.body });
